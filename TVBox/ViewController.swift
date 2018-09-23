6496a8507557a8fb99294e3e5669e21c67216d73
Vitaly Berg <bergusman@gmail.com>
2018-09-23 01:44:45 +0100

    Initial Commit

---
+++ TVBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVBox
+//
+//  Created by Vitaliy Berg on 23/09/2018.
+//  Copyright © 2018 Vitaly Berg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
